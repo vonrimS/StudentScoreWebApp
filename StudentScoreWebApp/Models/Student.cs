e14b9b2c5997f83fd9632956bc6d77a74ea199a5
vonrimS <mr.mytab@gmail.com>
2021-10-13 16:04:09 +0300

    Model, context, conn str

---
+++ StudentScoreWebApp/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentScoreWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        private string _FullName { get; set; }
+        public string FullName
+        {
+            get { return _FullName; }
+            set { _FullName = LastName + " " + FirstName + " " + SecondName; }
+        }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public int Score3 { get; set; }
+        public int Score4 { get; set; }
+        private int _TotalScore { get; set; }
+        public int TotalScore
+        {
+            get { return _TotalScore; }
+            set { _TotalScore = Score1 + Score2 + Score3 + Score4; }
+        }
+        public Student()
+        {
+            Score1 = Score2 = Score3 = Score4 = 0;
+        }
+    }
+}
---
+++ StudentScoreWebApp/Models/StudentContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentScoreWebApp.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+    public class StudentDbInitializer : DropCreateDatabaseAlways<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {          
+            context.Students.Add(new Student { LastName = "Азарин", FirstName = "Кирилл", SecondName = "Георгиевич" });
+            context.Students.Add(new Student { LastName = "Айвазян", FirstName = "Карен", SecondName = "Гарикович" });
+            context.Students.Add(new Student { LastName = "Бефус", FirstName = "Полина", SecondName = "Олеговна" });
+            context.Students.Add(new Student { LastName = "Говоров", FirstName = "Никита", SecondName = "Георгиевич" });
+            context.Students.Add(new Student { LastName = "Яценко", FirstName = "Кирилл", SecondName = "Александрович" });
+            context.Students.Add(new Student { LastName = "Горбунков", FirstName = "Семен", SecondName = "Сергеевич" });
+            context.Students.Add(new Student { LastName = "Куйбышев", FirstName = "Кирилл", SecondName = "Борисович" });
+            context.Students.Add(new Student { LastName = "Мурашко", FirstName = "Анна", SecondName = "Дмитриевна" });
+            context.Students.Add(new Student { LastName = "Куценко", FirstName = "Дарья", SecondName = "Романовна" });
+            context.Students.Add(new Student { LastName = "Лысин", FirstName = "Данил", SecondName = "Максимович" });
+            context.Students.Add(new Student { LastName = "Яровой", FirstName = "Николай", SecondName = "Валентинович" });
+            
+            context.SaveChanges();            
+        }
+    }
+}
